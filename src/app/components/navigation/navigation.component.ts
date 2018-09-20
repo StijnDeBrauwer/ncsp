5246079e5e59e3ab6d9a86ccdf35aa3d34f0ef61
StijnDeBrauwer <db_stijn@hotmail.com>
2018-09-20 13:11:03 +0200

    Removed unnecessary code + added service to determine if on mobile

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,17 +1,101 @@
-import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import {ContactPage} from '../../pages/company/contact/contact.page';
-import {EventsPage} from '../../pages/company/events/events.page';
-import {AboutUsPage} from '../../pages/company/about-us/about-us.page';
-import {OurCompanyComponent} from '../../pages/company/our-company/our-company.component';
-
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { ContactPage } from '../../pages/company/contact/contact.page';
+import { EventsPage } from '../../pages/company/events/events.page';
+import { AboutUsPage } from '../../pages/company/about-us/about-us.page';
+import { OurCompanyComponent } from '../../pages/company/our-company/our-company.component';
+import {ResponsiveService } from '../../services/responsive/responsive.service';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss'],
-    animations: [
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  navOpen = false;
+  subMenuOpen = false;
+  chosenLink: { text: string, path: string, children: any[] };
+  links: Array<{ text: string, path: string, children: any[] }> = [];
+
+
+  constructor(private router: Router, public el: ElementRef, private responsiveService: ResponsiveService) {
+
+    this.router.config.push(
+      {
+        path: 'company',
+        component: OurCompanyComponent,
+        children: [
+          { path: '', redirectTo: 'company', pathMatch: 'full' },
+          { path: 'about', component: AboutUsPage },
+          { path: 'contact', component: ContactPage }
+        ]
+      },
+    );
+
+    this.links.push(
+      {
+        text: 'OUR COMPANY', path: undefined, children: [
+          { text: 'ABOUT US', path: 'about' },
+          { text: 'CONTACT US', path: 'contact' },
+        ]
+      }
+    );
+  }
+
+  ngOnInit() {
+
+  }
+
+  navigate() {
+    this.navOpen = false;
+  }
+
+  moreOptions() {
+
+  }
+
+  toggle() {
+    this.navOpen = !this.navOpen;
+  }
+
+  toggleSubMenu(link) {
+    this.chosenLink = link;
+    this.subMenuOpen = !this.subMenuOpen;
+  }
+
+  chosenLinkExists(){
+    return this.links.includes(this.chosenLink);
+  }
+
+}
+
+
+/**
+navbarState = 'hide';
+logoState = 'big';
+  host HostListener:
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+      const scrollPosition = window.pageYOffset;
+
+      if (!this.navOpen) {
+          if (50 > scrollPosition) {
+              this.logoState = 'big';
+              this.navbarState = 'hide';
+
+          } else {
+              this.navbarState = 'show';
+              this.logoState = 'small';
+              // console.log('show');
+          }
+      }
+
+  }
+
+  //animations
+   animations: [
         trigger('scroll', [
             state('hide', style({backgroundColor: 'transparent', height: '100px'})),
             state('show', style({backgroundColor: '#ffffff', height: '75px'})),
@@ -23,99 +107,5 @@ import {OurCompanyComponent} from '../../pages/company/our-company/our-company.c
             transition('small <=> big', animate('50ms')),
         ])
     ]
-})
-export class NavigationComponent implements OnInit {
 
-    navOpen = false;
-    subMenuOpen = false;
-    navbarState = 'hide';
-    logoState = 'big';
-
-    links: Array<{ text: string, path: string, children: any[] }> = [];
-
-
-    @HostListener('window:scroll', ['$event'])
-    checkScroll() {
-        const scrollPosition = window.pageYOffset;
-
-        if (!this.navOpen) {
-            if (50 > scrollPosition) {
-                this.logoState = 'big';
-                this.navbarState = 'hide';
-
-            } else {
-                this.navbarState = 'show';
-                this.logoState = 'small';
-                // console.log('show');
-            }
-        }
-
-    }
-
-    constructor(private router: Router, public el: ElementRef) {
-
-        this.router.config.push(
-            {
-                path: 'company',
-                component: OurCompanyComponent,
-                children: [
-                    {path: '', redirectTo: 'company', pathMatch: 'full'},
-                    {path: 'about', component: AboutUsPage},
-                    {path: 'contact', component: ContactPage}
-                ]
-            },
-        );
-
-        this.links.push(
-            {
-                text: 'OUR COMPANY', path: undefined, children: [
-                    {text: 'ABOUT US', path: 'about'},
-                    {text: 'CONTACT US', path: 'contact'},
-                ]
-            }
-        );
-        // this.router.config.unshift(
-        //     {
-        //         path: 'contact',
-        //         component: ContactPage
-        //     },
-        //     {
-        //         path: 'events',
-        //         component: EventsPage
-        //     },
-        //     {
-        //         path: 'about',
-        //         component: AboutUsPage
-        //     }
-        // );
-        //
-        //
-        // this.links.push(
-        //     {text: 'Events', path: 'events'},
-        //     {text: 'About Us', path: 'about'},
-        //     {text: 'Contact', path: 'contact'}
-        // );
-
-    }
-
-    ngOnInit() {
-
-    }
-
-    navigate() {
-        this.navOpen = false;
-    }
-
-    moreOptions() {
-
-    }
-
-    toggle() {
-        this.navOpen = !this.navOpen;
-    }
-
-    toggleSubMenu() {
-        this.subMenuOpen = !this.subMenuOpen;
-    }
-
-}
+*/
