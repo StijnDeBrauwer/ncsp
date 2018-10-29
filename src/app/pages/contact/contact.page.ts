583fc9a5806747e7b5037be674f2ed21d63a7736
StijnDeBrauwer <db_stijn@hotmail.com>
2018-10-29 19:14:13 +0100

    added mail form

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,5 +1,5 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-contact',
@@ -11,42 +11,31 @@ export class ContactPage implements OnInit {
     contactForm: FormGroup;
     disabledSubmitButton: boolean = true;
     optionsSelect: Array<any>;
+    @ViewChild('form') formEl;
 
-    @HostListener('input') oninput() {
-
-        if (this.contactForm.valid) {
-            this.disabledSubmitButton = false;
-        }
-    }
-
-    constructor(private fb: FormBuilder) {
-        this.contactForm = fb.group({
-            'contactFormName': ['', Validators.required],
-            'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
-            'contactFormSubjects': ['', Validators.required],
-            'contactFormMessage': ['', Validators.required],
-            'contactFormCopy': [''],
+    constructor() {
+        this.contactForm = new FormGroup({
+            fromName: new FormControl('', Validators.required),
+            fromEmail: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+            subject: new FormControl('', Validators.required),
+            message: new FormControl('', Validators.required)
         });
+
     }
 
     ngOnInit() {
-
-        this.optionsSelect = [
-            {value: 'Feedback', label: 'Feedback'},
-            {value: 'Report a bug', label: 'Report a bug'},
-            {value: 'Feature request', label: 'Feature request'},
-            {value: 'Other stuff', label: 'Other stuff'},
-        ];
     }
 
     onSubmit() {
-        // this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
-        //     alert('Your message has been sent.');
-        //     this.contactForm.reset();
-        //     this.disabledSubmitButton = true;
-        // }, error => {
-        //     console.log('Error', error);
-        // });
+        if (this.contactForm.errors) {
+            return;
+        }
+        const regex = /\r?\n|\r/g;
+        const currentText: string = String(this.contactForm.controls.message.value);
+        const body = currentText.replace(regex, '%0D%0A');
+        console.log(currentText);
+        this.formEl.nativeElement.action = "mailto:db_stijn@hotmail.com?subject=" + this.contactForm.controls.subject.value + "&body=Test";
+        this.formEl.nativeElement.submit()
     }
 
 }
